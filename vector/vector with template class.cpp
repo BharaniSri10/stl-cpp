c1e8a0978badd8f5d80407b508b9b75353d24e71
A BHARANI SRI <89077065+BharaniSri10@users.noreply.github.com>
2022-01-16 11:00:19 +0530

    Update vector with template class.cpp

--- vector/vector with template class.cpp
+++ vector/vector with template class.cpp
@@ -17,19 +17,14 @@ class Vector1{
 };
 
 int main(){
-/*	Vector1<int> ob1;
+	Vector1<int> ob1;
 	ob1.push(1);
 	ob1.push(2);	
 	ob1.push(3);
 	ob1.push(4);
 	ob1.push(5);
-	cout<<ob1.pop();*/
+	cout<<ob1.pop();
 		
 }
-/*void Vector::push(T val){
-	vec.push_back(val);
-}
-T Vector::pop(){
-	return vec.pop();
-}*/
-	
+
+
